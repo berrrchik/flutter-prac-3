375bba6f1c93cc55edd19655ddac68224cb443bd
berrrchik <berrrchik@mail.ru>
2025-10-25 02:20:16 +0300

    feat: add mood entry screen

---
+++ lib/add_mood_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddMoodScreen extends StatelessWidget {
+  const AddMoodScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Добавить запись настроения',
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
+
